8e33d24317ff9953ceaa79dad972ab9ed4b3f340
corbanpro <corbanpro@gmail.com>
2023-02-07 18:28:20 -0700

    Add project files.

---
+++ Mission6Assignment_cgp27/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Mission6Assignment_cgp27.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
